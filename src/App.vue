85a959ce592694ee2ac18b555756947f7f39a6f2
ToguyC <tanguy.cvgn@gmail.com>
2024-06-20 21:55:07 +0200

    update: aside width

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { TruckIcon } from '@heroicons/vue/24/outline';
+import { HomeIcon, TruckIcon } from '@heroicons/vue/24/outline';
 import { onMounted, ref } from 'vue';
 import Emergency from './components/Emergency.vue';
 import NavLink from './components/NavLink.vue';
@@ -19,12 +19,15 @@ onMounted(() => {
 
 <template>
     <div class="flex h-screen p-10 text-slate-900">
-        <aside class="h-full flex flex-col items-center gap-20 bg-slate-800 rounded-xl">
+        <aside class="h-full w-60 flex flex-col items-center gap-20 bg-slate-800 rounded-xl">
             <RouterLink to="/" class="pt-8">
                 <img src="/images/logo-2.png" alt="swisscat-logo" class="w-40" />
             </RouterLink>
 
             <nav class="w-full flex flex-col gap-5 text-white/50">
+                <NavLink to="/" name="Home">
+                    <HomeIcon class="w-10 h-10" />
+                </NavLink>
                 <NavLink to="/tasks" name="Tasks">
                     <TruckIcon class="w-10 h-10" />
                 </NavLink>
